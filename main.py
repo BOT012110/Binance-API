26a7e9818db58fb5e1b4f55dc25dea0c691c92de
Nurdin <nurdin@gmail.com>
2022-04-14 05:58:45 +0300

    binance api trading script

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from binance.client import Client
-from binance.enums import *
 import pandas as pa
 
-api_key = 'nNrfv2MweJDgY8eJCWAT9XbCrRj4E3n4pzgok7EgdBgfhFjvqoOGVJBgM4OniH05'
-api_secret = 'PVUiJMl5N4mar1WkP3WRwM7Z4Fh9JcB1jxnoT6e5lk0PHIBVr8h0g35pLVo28GV4'
+api_key = 'your_api_key'
+api_secret = 'your_api_secret'
 
 client = Client(api_key, api_secret)
 inf_log_n_st = client.get_system_status()
 print(f"Logged in. Status: {inf_log_n_st['status']}; message: {inf_log_n_st['msg']}")
 
-print("Commands: 'average'; 'buy' ; 'balance'; 'infpairs'")
+print("Commands: 'average' ; 'buy' ; 'balance' ; 'inf_pairs'")
 command = input()
 
 set_quantity = 0
@@ -31,21 +30,25 @@ def strategy(symbol, quantity, that=False):
     data_frame = info_about_pairs(symbol, '1m', '30')
     price_change = (data_frame.Open.pct_change() + 1).cumprod() - 1
     if not that:
-        if price_change[-1] > 0.002:
+        if price_change[-1] < -0.002:
             order = client.create_order(
                 symbol=symbol,
                 side='BUY',
                 type='MARKET',
                 quantity=quantity
             )
+
             print(order)
             that = True
         else:
-            print('No Trade? :(')
+            print('****** No Trade? :( ******')
+            print(that)
+
     if that:
         while True:
             data_frame = info_about_pairs(symbol, '1m', '30')
-            since_buy = data_frame.loc(data_frame.index > pa.to_datetime(order['transactTime'], unit='ms'))
+            since_buy = data_frame.loc[data_frame.index > pa.to_datetime(order['transactTime'], unit='ms')]
+
             if len(since_buy) > 0:
                 since_buy_ret = (since_buy.Open.pct_change() + 1).cumprod() - 1
                 if since_buy_ret[-1] > 0.0015 or since_buy_ret[-1] < 0.0015:
@@ -55,14 +58,14 @@ def strategy(symbol, quantity, that=False):
                         type='MARKET',
                         quantity=quantity
                     )
+
                     print(order)
                     break
 
 
 def average():
     while True:
-        print('Enter pairs to see their average price')
-        print('Enter "exit" to go to start')
+        print('****** Enter pairs to see their average price or write "exit" ******')
         avr_pairs = input()
         if avr_pairs == 'exit':
             break
@@ -70,7 +73,8 @@ def average():
         candles = client.get_avg_price(symbol=avr_pairs)
         print(candles)
 
-        print('See another?')
+        print('')
+        print('****** See another? ******')
         avg_yesno = input()
         if avg_yesno == 'yes':
             continue
@@ -97,14 +101,15 @@ while command != "Exit":
     if command == "average":
         average()
 
-    elif command == "infpairs":
+    elif command == "inf_pairs":
         infpairs()
 
     elif command == "buy":
-        print('Do you want to buy Crypto?')
+        print('****** Do you want to buy Crypto? ******')
         action = input()
         if action == 'yes':
             current_pairs = input("Enter pairs: ")
+            print("-------------------")
             quantity_qw = input('Enter quantity: ')
         else:
             break
@@ -114,59 +119,7 @@ while command != "Exit":
         balance()
 
     else:
-        print("***** Wrong command *****")
+        print("***** Wrong command!!! *****")
 
-    command = input("Enter command ")
-
-# while True:
-#     see_pair_yesno = (input('Want to average pair price (no or yes): '))
-#     if see_pair_yesno == 'yes':
-#         avr_pairs = input('Set pairs to see average price (like "SOLBNB"): ')
-#         candles = client.get_avg_price(symbol=avr_pairs)
-#         print(candles)
-#
-#         avg_yesno = (input('See another? '))
-#         if avg_yesno == 'yes':
-#             continue
-#         if avg_yesno == 'no':
-#             break
-#     if see_pair_yesno == 'no':
-#         break
-
-# while True:
-#     action = input('Do you want buy Crypto? ')
-#     if action == 'yes':
-#         print('Enter "exit" to go to start')
-#         current_pairs = input('Enter pairs (like "SOLBNB"): ')
-#         if current_pairs == 'exit':
-#             break
-#
-#         print('Use average price?')
-#         use_avg_price = input()
-#
-#         if use_avg_price:
-#             candles = client.get_avg_price(symbol=current_pairs)
-#             set_price = candles['price']
-#         else:
-#             set_price = float(input('Enter price: '))
-#             print('Enter "exit" to go to start')
-#             if set_price == 'exit':
-#                 break
-#
-#         print("U can set max quantity according to your balance or set it manually")
-#         choose_qunat = input('Enter "max" or "mnl": ')
-#         if choose_qunat == 'max':
-#             candles = client.get_avg_price(symbol=current_pairs)
-#             balance = client.get_asset_balance(asset=current_pairs)
-#             max_quantity = float(balance['free']) / float(candles['price'])
-#             set_quantity = round(max_quantity, 2)
-#         else:
-#             set_quantity = input("Enter quantity: ")
-#
-#         order = client.order_limit(
-#             symbol=current_pairs,
-#             quantity=set_quantity,
-#             price=set_price
-#         )
-#     if action == 'no':
-#         break
+    print("***** Enter command *****")
+    command = input()
